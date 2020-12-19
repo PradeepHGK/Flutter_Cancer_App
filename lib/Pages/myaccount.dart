0d5de7f9cf576ff1013c0cfd00d0720bd98603e6
PradeepHGK <pradeep.hgk@ziniosedge.com>
2020-12-20 00:00:56 +0530

    method seggregated

--- lib/Pages/myaccount.dart
+++ lib/Pages/myaccount.dart
@@ -7,62 +7,74 @@ class Myaccount extends StatelessWidget {
       color: Colors.orange[800],
       child: Stack(
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                height: 300,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
+          backgroundDesign(context),
+          topPanelContent(context)
+        ],
+      ),
+    );
+  }
+
+  Padding topPanelContent(BuildContext context) {
+    return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child:
+              Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            SizedBox(
+              height: 120,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(color: Colors.black45, blurRadius: 10)
+                  ],
                   color: Colors.white,
-                ),
-              )
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              SizedBox(
-                height: 120,
+                  borderRadius: BorderRadius.circular(25)),
+              height: 500,
+              width: MediaQuery.of(context).size.width,
+              child: textButton(),
+            ),
+          ]),
+        );
+  }
+
+  Column backgroundDesign(BuildContext context) {
+    return Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              height: 300,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
               ),
-              Container(
-                decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(color: Colors.black45, blurRadius: 10)
-                    ],
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(25)),
-                height: 500,
-                width: MediaQuery.of(context).size.width,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            "SignIn",
-                            style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.orange[800]),
-                          )),
-                      TextButton(
-                          onPressed: () {},
-                          child: Text("SignUp",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey[400])))
-                    ],
-                  ),
-                ),
-              ),
-            ]),
-          )
+            )
+          ],
+        );
+  }
+
+  Padding textButton() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextButton(
+              onPressed: () {},
+              child: Text(
+                "SignIn",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.orange[800]),
+              )),
+          TextButton(
+              onPressed: () {},
+              child: Text("SignUp",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[400])))
         ],
       ),
     );
