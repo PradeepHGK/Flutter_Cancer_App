0f80b52ca7167c159fdb68c8b6e8b089a24664ba
PradeepHGK <pradeep.hgk@ziniosedge.com>
2020-12-21 18:55:09 +0530

    patients expansion tile added

--- lib/Pages/Patients.dart
+++ lib/Pages/Patients.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lorem_ipsum/lorem_ipsum.dart';
 
 class Patients extends StatelessWidget {
   @override
@@ -19,6 +20,96 @@ class Patients extends StatelessWidget {
           height: 10,
         ),
         patientScrollList(),
+        Expanded(
+            child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      elevation: 20,
+                      child: ExpansionTile(
+                          leading: Icon(Icons.local_hospital),
+                          maintainState: false,
+                          backgroundColor: Colors.white24,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Text(loremIpsum(paragraphs: 1)),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      FlatButton(
+                                          onPressed: () {
+                                            print("Call Pressed");
+                                          },
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                                color: Colors.deepOrange,
+                                                borderRadius:
+                                                    BorderRadius.circular(5)),
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceEvenly,
+                                              children: [
+                                                Icon(
+                                                  Icons.call,
+                                                  color: Colors.white,
+                                                ),
+                                                Text(
+                                                  "Call",
+                                                  style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontWeight:
+                                                          FontWeight.bold),
+                                                )
+                                              ],
+                                            ),
+                                            height: 50,
+                                            width: 100,
+                                          )),
+                                      FlatButton(
+                                          onPressed: () {
+                                            print("Donate Pressed");
+                                          },
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                                color: Colors.deepOrange,
+                                                borderRadius:
+                                                    BorderRadius.circular(5)),
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceEvenly,
+                                              children: [
+                                                Icon(
+                                                  Icons.donut_large_sharp,
+                                                  color: Colors.white,
+                                                ),
+                                                Text(
+                                                  "Donate",
+                                                  style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontWeight:
+                                                          FontWeight.bold),
+                                                )
+                                              ],
+                                            ),
+                                            height: 50,
+                                            width: 100,
+                                          )),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                          title: Text("Patient details")),
+                    ),
+                  );
+                }))
       ],
     );
   }
@@ -32,64 +123,68 @@ class Patients extends StatelessWidget {
           scrollDirection: Axis.horizontal,
           itemCount: 5,
           itemBuilder: (context, index) {
-            return Container(
-              color: Colors.transparent,
-              padding: EdgeInsets.all(5.0),
-              height: 250,
-              width: 300,
-              child: Stack(
-                children: [
-                  ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Image(image: AssetImage("images/homepage.jpeg"))),
-                  Positioned(
-                    left: 10,
-                    bottom: 10,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
-                          color: Colors.white30),
-                      height: 50,
-                      width: 180,
-                      child: Padding(
-                        padding: const EdgeInsets.all(2.0),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Icon(
-                              Icons.person,
-                              color: Colors.white,
-                            ),
-                            Column(
-                              children: [
-                                Text(
-                                  "Ram",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20,
-                                  ),
-                                ),
-                                Text(
-                                  "Lungs Cancer",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w500),
-                                )
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            );
+            return horizontalPatientlist();
           }),
+    );
+  }
+
+  Container horizontalPatientlist() {
+    return Container(
+      color: Colors.transparent,
+      padding: EdgeInsets.all(5.0),
+      height: 250,
+      width: 300,
+      child: Stack(
+        children: [
+          ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image(image: AssetImage("images/homepage.jpeg"))),
+          Positioned(
+            left: 10,
+            bottom: 10,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white30),
+              height: 50,
+              width: 180,
+              child: Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.person,
+                      color: Colors.white,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Ram",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                        Text(
+                          "Lungs Cancer",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
