2365d5afc1567d4326d58d61264dce92ab480919
PradeepHGK <pradeep.hgk@ziniosedge.com>
2020-12-29 10:35:05 +0530

    Hospital page parent widge expception cleared

--- lib/Pages/Hospital/Hospital_Details.dart
+++ lib/Pages/Hospital/Hospital_Details.dart
@@ -1,6 +1,7 @@
 import 'package:demoapp/Pages/Hospital/Hospitals.dart';
 import 'package:demoapp/Pages/homePage.dart';
 import 'package:flutter/material.dart';
+import 'package:lorem_ipsum/lorem_ipsum.dart';
 
 class Hospital_Details extends StatefulWidget {
   @override
@@ -12,79 +13,111 @@ class _Hospital_DetailsState extends State<Hospital_Details> {
   Widget build(BuildContext context) {
     return Material(
       child: SafeArea(
-        child: Expanded(
-          child: Container(
-            child: Stack(children: [
-              Expanded(
-                child: Stack(children: [
-                  Container(
-                      height: MediaQuery.of(context).size.height / 2,
-                      child: Image(
-                        fit: BoxFit.fill,
-                        image: AssetImage("images/homepage.jpeg"),
-                      )),
-                  IconButton(
-                      icon: Icon(
-                        Icons.arrow_back,
-                        size: 30,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => HomeScreen()));
-                      }),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Stack(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                              color: Colors.yellow[300],
-                              borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(20),
-                                  topRight: Radius.circular(20))),
-                          height: 500,
-                          width: 500,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 20, vertical: 20),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
+        child: Container(
+          child: Stack(children: [
+            Stack(children: [
+              Container(
+                  height: MediaQuery.of(context).size.height / 2,
+                  child: Image(
+                    fit: BoxFit.fill,
+                    image: AssetImage("images/homepage.jpeg"),
+                  )),
+              IconButton(
+                  icon: Icon(
+                    Icons.arrow_back,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => HomeScreen()));
+                  }),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.yellow[300],
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(20))),
+                      height: 500,
+                      width: 500,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
                               children: [
                                 Text(
-                                  "Heading",
+                                  loremIpsum(words: 3),
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                                Text("Subtitle"),
-                                Text(
-                                  "Heading",
-                                ),
-                                Text("Subtitle"),
+                                Stack(
+                                  children: [
+                                    CircleAvatar(
+                                        backgroundColor: Colors.white,
+                                        child: Transform.rotate(
+                                          angle: 45,
+                                          child: IconButton(
+                                              tooltip: "LocationButton",
+                                              icon: Icon(
+                                                Icons.navigation_rounded,
+                                                color: Colors.blue,
+                                              ),
+                                              onPressed: () {
+                                                print("NavigationButton");
+                                              }),
+                                        ))
+                                  ],
+                                )
                               ],
                             ),
-                          ),
+                            Text(
+                              "City",
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "Address",
+                            ),
+                            Text("Subtitle"),
+                          ],
                         ),
-                        Positioned(
-                          bottom: 0,
-                          child: Container(
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(20),
-                                    topRight: Radius.circular(20))),
-                            height: MediaQuery.of(context).size.height / 2 - 50,
-                            width: MediaQuery.of(context).size.width,
-                          ),
-                        )
-                      ],
+                      ),
                     ),
-                  )
-                ]),
-              ),
+                    Positioned(
+                      bottom: 0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20))),
+                        height: MediaQuery.of(context).size.height / 2 - 50,
+                        width: MediaQuery.of(context).size.width,
+                      ),
+                    )
+                  ],
+                ),
+              )
             ]),
-          ),
+          ]),
         ),
       ),
     );
