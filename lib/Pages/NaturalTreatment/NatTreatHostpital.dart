c6941d4b446fd6a6f97ae39c75f76d28e940550e
PradeepHGK <pradeep.hgk@ziniosedge.com>
2021-01-12 18:44:43 +0530

    nat hospital page updated

--- lib/Pages/NaturalTreatment/NatTreatHostpital.dart
+++ lib/Pages/NaturalTreatment/NatTreatHostpital.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:lorem_ipsum/lorem_ipsum.dart';
 
 class NatTreatHostpital extends StatefulWidget {
   @override
@@ -7,54 +10,127 @@ class NatTreatHostpital extends StatefulWidget {
 
 //Name, Address, Location, Image, Comments
 class _NatTreatHostpitalState extends State<NatTreatHostpital> {
-  Widget bottomSheetContainer() {
-    return Container(
-      alignment: Alignment.bottomCenter,
-      height: 100,
-      width: MediaQuery.of(context).size.width,
-      color: Colors.green,
-      child: Row(
-        children: [
-          IconButton(
-            color: Colors.white,
-            icon: Icon(Icons.camera),
-            onPressed: () {},
-          )
-        ],
-      ),
-    );
+  bool isDarkModeEnabled;
+  bool saveDarkState;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    print("SaveDarkState $saveDarkState");
+    isDarkModeEnabled = saveDarkState != null ? saveDarkState : false;
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios),
-            color: Colors.black,
-            onPressed: () {}),
-        backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        color: Colors.white,
-        child: Column(
-          children: [
-            GestureDetector(
-              onTap: () {
-                showBottomSheet(
-                    context: context,
-                    builder: (builder) => bottomSheetContainer());
-              },
-              child: Container(
-                color: Colors.greenAccent,
-                height: 100,
-                width: 100,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: AnimatedContainer(
+          duration: Duration(milliseconds: 500),
+          curve: Curves.easeInSine,
+          decoration: BoxDecoration(
+              color: isDarkModeEnabled ? HexColor("#222831") : Colors.white,
+              borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(10),
+                  bottomLeft: Radius.circular(10))),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                        color: isDarkModeEnabled ? Colors.white : Colors.black,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          print(isDarkModeEnabled);
+                          saveDarkState = isDarkModeEnabled;
+                        });
+                        Navigator.pop(context);
+                      }),
+                  IconButton(
+                      icon: Icon(
+                        Icons.lightbulb_outline_rounded,
+                        color: isDarkModeEnabled ? Colors.white : Colors.black,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          isDarkModeEnabled = !isDarkModeEnabled;
+                        });
+                      })
+                ],
               ),
-            )
-          ],
+              Expanded(
+                child: AnimatedContainer(
+                  curve: Curves.easeInSine,
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  color: isDarkModeEnabled ? HexColor("#222831") : Colors.white,
+                  duration: Duration(milliseconds: 500),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            loremIpsum(words: 2),
+                            textAlign: TextAlign.left,
+                            style: GoogleFonts.allura(
+                                fontWeight: FontWeight.bold,
+                                color: isDarkModeEnabled
+                                    ? Colors.white
+                                    : Colors.black,
+                                fontSize: 25),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(loremIpsum(paragraphs: 2),
+                            style: GoogleFonts.actor(
+                              fontSize: 15,
+                              color: isDarkModeEnabled
+                                  ? Colors.white
+                                  : Colors.black,
+                              // color: Color(222831)
+                            )),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: HexColor("374045"),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 30),
+                            child: ListView.builder(
+                              physics: BouncingScrollPhysics(),
+                              itemCount: 4,
+                              itemBuilder: (BuildContext context, int index) {
+                                return Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: ClipRRect(
+                                    child: Image.asset("images/homepage.jpeg"),
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
